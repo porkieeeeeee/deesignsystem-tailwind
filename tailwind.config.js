7327c09eeefcc0a04311805db931b672edcd2057
porkieeeeeee <porkieeeeeee@gmail.com>
2024-06-17 17:45:42 +0900

    init::환경세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,61 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    corePlugins: {
+        preflight: true,
+    },
+    purge: [],
+    darkMode: false, 
     theme: {
+        colors: {
+            primary: "#4c6ef5",
+            gray000: "#fff",
+            gray100: "#495057",
+            gray200: "#292e33",
+            gray300: "#212529",
+        },
+        fontWeight: {
+            'regular': '400',
+            'medium': '500',
+            'semibold': '600',
+            'bold': '700',
+        },
+        fontFace: {
+            'Pretendard': [
+              {
+                fontWeight: '400',
+                src: [
+                  'url("assets/fonts/Pretendard-Regular.woff2") format("woff2")',
+                  'url("assets/fonts/Pretendard-Regular.woff") format("woff")'
+                ],
+                fontDisplay: 'swap'
+              },
+              {
+                fontWeight: '500',
+                src: [
+                  'url("assets/fonts/Pretendard-Medium.woff2") format("woff2")',
+                  'url("assets/fonts/Pretendard-Medium.woff") format("woff")'
+                ],
+                fontDisplay: 'swap'
+              },
+              {
+                fontWeight: '600',
+                src: [
+                  'url("assets/fonts/Pretendard-SemiBold.woff2") format("woff2")',
+                  'url("assets/fonts/Pretendard-SemiBold.woff") format("woff")'
+                ],
+                fontDisplay: 'swap'
+              },
+              {
+                fontWeight: '700',
+                src: [
+                  'url("assets/fonts/Pretendard-Bold.woff2") format("woff2")',
+                  'url("assets/fonts/Pretendard-Bold.woff") format("woff")'
+                ],
+                fontDisplay: 'swap'
+              }
+            ]
+          }
+    },
+    variants: {
         extend: {},
     },
     plugins: [],
